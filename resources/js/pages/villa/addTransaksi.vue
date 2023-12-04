c71d0655380c5237152a55e3b5b3059f90991793
amiminn <tholabul.amin@gmail.com>
2023-12-04 15:37:41 +0700

    tab transaksi

--- resources/js/pages/villa/addTransaksi.vue
+++ resources/js/pages/villa/addTransaksi.vue
@@ -1,5 +1,14 @@
 <template lang="">
-    <div></div>
+    <card>
+        <form @submit.prevent="submitForm">
+            <div class="grid sm:xl:grid-cols-2 gap-3">
+                <div class=""></div>
+            </div>
+            <button type="submit" class="btn btn-danger btn-block">
+                submit
+            </button>
+        </form>
+    </card>
 </template>
 <script>
 export default {
